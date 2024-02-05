0b592eb9829ab8af6d30758f6378c4d805cd49f9
Crystal Wicks <cwi340@uky.edu>
2024-02-04 23:18:21 -0500

    Make main.cpp interactive
    Made program interactive. Program now asks user to enter their name and
    then displays a greeting to the user.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 # include <iostream>
 
 int main() {
+    //get user's name
+    std::cout << "Please enter your name: ";
+    std::string name;
+    std::getline(std::cin, name);
+    
     //display a greeting
-    std::cout << "Hello World." << std::endl;
+    std::cout << "Hello " << name << "!" << std::endl;
 
     return 0;
 }
